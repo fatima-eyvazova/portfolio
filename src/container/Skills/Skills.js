808dcf5f1f40d3c91b58e25a84b5f88c235d57a7
Fatima Eyvazova <fatima.eyvazovaa@gmail.com>
2023-05-29 20:00:37 +0400

    initial commit

---
+++ src/container/Skills/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Skills/Skills.scss'
+const Skills = () => {
+    return (
+        <div>
+            Skills
+        </div>
+    )
+}
+
+export default Skills
---
+++ src/container/Testimonial/Testimonial.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Testimonial/Testimonial.scss'
+const Testimonial = () => {
+    return (
+        <div>
+            Testimonial
+        </div>
+    )
+}
+
+export default Testimonial
